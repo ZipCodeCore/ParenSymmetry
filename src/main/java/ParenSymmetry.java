f8a7926a0dbfe14b37ce6954ee301bee02527464
Kristofer <kris@youngers.org>
2024-03-05 15:31:13 -0500

    added new unit tests

--- src/main/java/ParenSymmetry.java
+++ src/main/java/ParenSymmetry.java
@@ -1,7 +1,9 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class ParenSymmetry {
 
     public Boolean isBalanced(String s) {
-        // implement this method
         return null;
     }
 
--- src/test/java/ParenSymmetryTest.java
+++ src/test/java/ParenSymmetryTest.java
@@ -41,5 +41,25 @@ public class ParenSymmetryTest {
         actual = new ParenSymmetry().isBalanced(inputTest);
         Assert.assertEquals(expected, actual);
     }
+    @Test
+    public void isBalancedFalses() {
+        String[] falseStrings = {"(", "((", ")", "(()())((())))"};
+        Boolean expected = false;
+        Boolean actual;
+        for (String testString : falseStrings) {
+            actual = new ParenSymmetry().isBalanced(testString);
+            Assert.assertEquals(expected, actual);
+        }
+    }
+    @Test
+    public void isBalancedTrues() {
+        String[] trueStrings = {"()", "(())", "(((())))", "", "(()())((()))", "(   )", "( () ( ) )"};
+        Boolean expected = true;
+        Boolean actual;
+        for (String testString : trueStrings) {
+            actual = new ParenSymmetry().isBalanced(testString);
+            Assert.assertEquals(expected, actual);
+        }
+    }
 
 }
